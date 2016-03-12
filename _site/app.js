1432f7280ea63795d5910fc25c573ba8a7aef7b4
Dan Hammer <dan.s.hammer@gmail.com>
2016-03-12 14:34:06 -0800

    new website

--- _site/app.js
+++ _site/app.js
@@ -2,19 +2,38 @@ function currentUser() {
     return currentEndpoint().match(/http[s]*:\/\/([^.]*).*/)[1];
 }
 
-function currentEndpoint() {
-    return "http://wri-01.cartodb.com/api/v1/map";
-}
 
-function render(year, coords,layer,style,spinner){
+function render(year, coords, layer, style, spinner){
     var target = document.getElementById('map');
         spinner.spin(target);
 
-        console.log(year);
-    };
+            var xmin = coords[0].lng;
+            var ymin = coords[0].lat;
+            var xmax = coords[3].lng;
+            var ymax = coords[2].lat;
+            $.ajax({
+                dataType: "json",
+                url: "http://waterapp.enviro-service.appspot.com/crops?xmin="+xmin+"&xmax="+xmax+"&ymin="+ymin+"&ymax="+ymax+"&year="+year,
+                    success: function(data) {
+                        document.getElementById('texter').innerHTML = '<i>What covers this land?</i>    ' 
+
+                        var sorted = data.results.sort(function(x, y) {
+                            return parseFloat(y.area) - parseFloat(x.area)
+                        });
+
+                        res = 'In '+year+', <b>'+sorted[0]['landType']+'</b> ('+sorted[0]['area']+' acres) and <b>'+sorted[1]['landType']+'</b> ('+sorted[1]['area']+' acres).'
+
+                        document.getElementById('texter').innerHTML = '<i>What covers this land?</i>    ' + res
+                        spinner.stop(target);
+                    }
+            }).error(function() {spinner.stop(target);});
+    }
+
+
 
 
 function main() {
+     $('#yearcontrol');
 
      $('#controls').on('click dblclick mousedown mousewheel', function(e) {
         e.stopPropagation();
@@ -42,21 +61,38 @@ function main() {
         };
         var target = document.getElementById('map');
         var spinner = new Spinner(opts);
+      // Disable dragging when user's cursor enters the element
+    document.querySelector('#yearcontrol').addEventListener('mouseover', function () {
+        map.dragging.disable();
+    });
+
+    // Re-enable dragging when user's cursor leaves the element
+    document.querySelector('#yearcontrol').addEventListener('mouseout', function () {
+        map.dragging.enable();
+    });
+     
+
+    var pol_pgis = null;
+    var year=2008;
+    //slider.defaultValue=year;
+    //slider.value=year;
+    slider.onchange = function(){
+    document.querySelector('#currentYear').value = this.value;
+    year=this.value;
+    render(year, pol_pgis, boundary, waterStyle,spinner)
+    }
     
     // Create map
     var map = new L.Map('map', {
         zoomControl: true,
         drawnControl: true,
-        center: [37.677816, -121.372880],
+        center: [37.227638, -120.541387],
         zoom: 13
     });
 
     // Add CartoDB basemaps
-    // https://cartodb-basemaps-a.global.ssl.fastly.net/light_nolabels
-    // https://1.maps.nlp.nokia.com/maptile/2.1/maptile/newest/satellite.day
-    // http://api.mapbox.com/v4/mapbox.streets/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiZGFuaGFtbWVyIiwiYSI6IkpyY19CNFkifQ.2Y7Un3COo3E_81ROBLKkSg
     L.tileLayer('http://api.mapbox.com/v4/mapbox.satellite/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiZGFuaGFtbWVyIiwiYSI6IkpyY19CNFkifQ.2Y7Un3COo3E_81ROBLKkSg', {
-        attribution: '<a href="http://www.earthgenome.org">Earth Genome</a> © 2015',
+        attribution: '<a href="http://danham.me/r">Hammer</a> © 2015',
         maxZoom: 18
     }).addTo(map);
 
@@ -86,7 +122,7 @@ function main() {
                 shapeOptions: {
                     color: '#a63b55',
                     fill:false,
-                    weight:3.5
+                    weight:2
                 },
                 showArea: true
             },
@@ -114,57 +150,44 @@ function main() {
             // Create a Rectangle geometry in PostGIS
             case 'rectangle':
                 var coords = layer.getLatLngs();
-                
-
-                pol_pgis = "[["+ 
-                    coords[0].lng +","+coords[0].lat+"],["+ 
-                    coords[1].lng +","+coords[1].lat+"],["+ 
-                    coords[2].lng +","+coords[2].lat+"],["+ 
-                    coords[3].lng +","+coords[3].lat+"]]";
-               
+                pol_pgis = coords
                 break;
             
         }
+
 
         if (pol_pgis) {
             var xmin = coords[0].lng;
             var ymin = coords[0].lat;
             var xmax = coords[3].lng;
             var ymax = coords[2].lat;
-
-            console.log("http://localhost:8080/crops?xmin="+xmin+"&xmax="+xmax+"&ymin="+ymin+"&ymax="+ymax+"&year=2014");
+            
+            //var year=2001;
+            drawnItems.clearLayers(); 
+            spinner.spin(target)
 
             $.ajax({
                 dataType: "json",
-                url: "http://localhost:8080/crops?xmin="+xmin+"&xmax="+xmax+"&ymin="+ymin+"&ymax="+ymax+"&year=2014",
+                url: "http://waterapp.enviro-service.appspot.com/crops?xmin="+xmin+"&xmax="+xmax+"&ymin="+ymin+"&ymax="+ymax+"&year="+year,
                     success: function(data) {
-                        console.log(data);
-                        document.getElementById('taggers').innerHTML = ''
-                        var div = document.getElementById('taggers');
-                        document.getElementById('taggers').style.fontSize = "18px"
+
+                        document.getElementById('texter').innerHTML = '<i>What covers this land?</i> ' 
 
                         var sorted = data.results.sort(function(x, y) {
                             return parseFloat(y.area) - parseFloat(x.area)
                         });
-                        var croptext = "";
-                        for (i = 0; i < 3; i++) {
-                            croptext += sorted[i]['area'] + ' acres of ' + sorted[i]['landType'] + ', ';
-                        }
-                        croptext += 'and ' + sorted[3]['area'] + ' acres of ' + sorted[3]['landType'] + '.';
-                        text1 = 'The drawn area is ' + data.totalAcreage + ' acres.  In 2014, there were ' + data.count + ' crops planted in the area, including ';
-                        div.innerHTML = text1 + croptext;
+
+                        res = 'In '+year+', <b>'+sorted[0]['landType']+'</b> ('+sorted[0]['area']+' acres) and <b>'+sorted[1]['landType']+'</b> ('+sorted[1]['area']+' acres).'
+
+                        document.getElementById('texter').innerHTML = '<i>What covers this land?</i>    ' + res
+                        spinner.stop(target);
                     }
-            });
+            }).error(function(errors) {spinner.stop(target);console.log (errors.statustext)});
+
 
         }
 
 
-
-        
-        else {
-            layer.bindPopup("Could not get value!");
-        }
-        
         drawnItems.addLayer(layer);
     });
 
