ac4082059f447ab53f010dc83a479b21a6c46f57
Dan Hammer <dan.s.hammer@gmail.com>
2016-03-12 13:29:05 -0800

    recent stripped version

--- _site/app.js
+++ _site/app.js
@@ -11,31 +11,12 @@ function render(year, coords,layer,style,spinner){
         spinner.spin(target);
 
         console.log(year);
-            $.ajax({
-                dataType: "json",
-                url: "http://vizz.water-test.appspot.com/water?coords="+coords+"&date="+year+"-01-01",
-                    success: function(data) {
-                        console.log(data);
-                        layer.clearLayers(); 
-                        //drawnItems.clearLayers(); 
-
-                        $(data.result.features).each(function(key, data) {
-                        layer.addData(data);
-                        layer.setStyle(style);
-                        spinner.stop(target);
-                        });
-                    }
-            }).error(function() {spinner.stop(target);});
-    }
-
-
+    };
 
 
 function main() {
-     $('#yearcontrol').hide();
 
      $('#controls').on('click dblclick mousedown mousewheel', function(e) {
-        console.log('hey');
         e.stopPropagation();
      });
 
@@ -61,39 +42,21 @@ function main() {
         };
         var target = document.getElementById('map');
         var spinner = new Spinner(opts);
-      // Disable dragging when user's cursor enters the element
-    document.querySelector('#yearcontrol').addEventListener('mouseover', function () {
-        map.dragging.disable();
-    });
-
-    // Re-enable dragging when user's cursor leaves the element
-    document.querySelector('#yearcontrol').addEventListener('mouseout', function () {
-        map.dragging.enable();
-    });
-     
-
-    var pol_pgis = null;
-    var year=1999;
-    //slider.defaultValue=year;
-    //slider.value=year;
-    slider.onchange = function(){
-    console.log(this.value);
-    document.querySelector('#currentYear').value = this.value;
-    year=this.value;
-    render(year, pol_pgis, boundary, waterStyle,spinner)
-    }
     
     // Create map
     var map = new L.Map('map', {
         zoomControl: true,
         drawnControl: true,
-        center: [37.591967, -121.547818],
-        zoom: 10
+        center: [37.677816, -121.372880],
+        zoom: 13
     });
 
     // Add CartoDB basemaps
-    L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
-        attribution: '<a href="http://danham.me/r">Dan Hammer</a> © 2015',
+    // https://cartodb-basemaps-a.global.ssl.fastly.net/light_nolabels
+    // https://1.maps.nlp.nokia.com/maptile/2.1/maptile/newest/satellite.day
+    // http://api.mapbox.com/v4/mapbox.streets/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiZGFuaGFtbWVyIiwiYSI6IkpyY19CNFkifQ.2Y7Un3COo3E_81ROBLKkSg
+    L.tileLayer('http://api.mapbox.com/v4/mapbox.satellite/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiZGFuaGFtbWVyIiwiYSI6IkpyY19CNFkifQ.2Y7Un3COo3E_81ROBLKkSg', {
+        attribution: '<a href="http://www.earthgenome.org">Earth Genome</a> © 2015',
         maxZoom: 18
     }).addTo(map);
 
@@ -123,7 +86,7 @@ function main() {
                 shapeOptions: {
                     color: '#a63b55',
                     fill:false,
-                    weight:0.5
+                    weight:3.5
                 },
                 showArea: true
             },
@@ -169,96 +132,34 @@ function main() {
             var xmax = coords[3].lng;
             var ymax = coords[2].lat;
 
-            console.log("http://localhost:8080/tagger?xmin="+xmin+"&xmax="+xmax+"&ymin="+ymin+"&ymax="+ymax)
+            console.log("http://localhost:8080/crops?xmin="+xmin+"&xmax="+xmax+"&ymin="+ymin+"&ymax="+ymax+"&year=2014");
 
             $.ajax({
                 dataType: "json",
-                url: "http://localhost:8080/tagger?xmin="+xmin+"&xmax="+xmax+"&ymin="+ymin+"&ymax="+ymax,
+                url: "http://localhost:8080/crops?xmin="+xmin+"&xmax="+xmax+"&ymin="+ymin+"&ymax="+ymax+"&year=2014",
                     success: function(data) {
                         console.log(data);
-                        spinner.stop(target);
+                        document.getElementById('taggers').innerHTML = ''
+                        var div = document.getElementById('taggers');
+                        document.getElementById('taggers').style.fontSize = "18px"
+
+                        var sorted = data.results.sort(function(x, y) {
+                            return parseFloat(y.area) - parseFloat(x.area)
+                        });
+                        var croptext = "";
+                        for (i = 0; i < 3; i++) {
+                            croptext += sorted[i]['area'] + ' acres of ' + sorted[i]['landType'] + ', ';
                         }
-                    
+                        croptext += 'and ' + sorted[3]['area'] + ' acres of ' + sorted[3]['landType'] + '.';
+                        text1 = 'The drawn area is ' + data.totalAcreage + ' acres.  In 2014, there were ' + data.count + ' crops planted in the area, including ';
+                        div.innerHTML = text1 + croptext;
+                    }
             });
 
-
-        }
-
-        if (pol_pgis) {
-            //var year=2001;
-            drawnItems.clearLayers(); 
-            spinner.spin(target)
-
-            $.ajax({
-                dataType: "json",
-                url: "http://vizz.water-test.appspot.com/water?coords="+pol_pgis+"&date="+year+"-01-01",
-                    success: function(data) {
-                        console.log(data);
-                        boundary.clearLayers(); 
-                        
-
-
-                        $(data.result.features).each(function(key, data) {
-                        boundary.addData(data);
-                        boundary.setStyle(waterStyle);
-                        $('#yearcontrol').fadeIn('slow');
-                        
-                        //year++;
-                        //year=(year>2011)?1999:year;
-                        spinner.stop(target);
-                        });
-                    }
-            }).error(function(errors) {spinner.stop(target);console.log (errors.statustext)});
-
-
         }
 
 
-        if (pol_pgis) {
 
-            $.ajax({
-                dataType: "json",
-                url: 'data.json',
-                // url: "http://vizz.water-test.appspot.com/water/series?coords="+pol_pgis+"&begin=2000-01-01&end=2014-01-01",
-                    success: function(data) {
-                        console.log(data);
-                        res = [];
-                        $(data.result).each(function(key, data) {
-                                res.push([Date.parse(data.date), data.ma, data.smooth]);
-                            }   
-                        )
-
-                        g = new Dygraph(
-                            document.getElementById("series"),
-                            res,
-                                {
-                                    ylabel: '',
-                                    drawXGrid: false,
-                                    drawYGrid: false,
-                                    colors: ["#808080", "#5F94D9"],
-                                    fillGraph: false,
-                                    drawPoints: false,
-                                    labels: ['Date', 'raw', 'smoothed'],
-                                    axes: {
-                                        x: {
-                                            valueFormatter: Dygraph.dateString_,
-                                            axisLabelFormatter: Dygraph.dateAxisFormatter,
-                                            ticker: Dygraph.dateTicker
-                                        }
-                                    },
-                                    drawXAxis: false,
-                                    height: 80,
-                                    width: 651,
-                                    rightGap: 10,
-                                    interactionModel: {}
-                                }
-
-                        )
-                    }
-            }).error(function(errors) {spinner.stop(target);console.log (errors.statustext)});
-
-
-        }
         
         else {
             layer.bindPopup("Could not get value!");
